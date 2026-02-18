10b0bf47bdbdaef504e8568199534c2e553eb8eb
Alex Andres <andres.alex@protonmail.com>
2026-02-18 14:36:22 +0100

    chore: add Babel configuration and update dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,11 @@ export default {
     testEnvironment: 'jsdom',
     roots: ['<rootDir>'],
     moduleFileExtensions: ['js'],
-    transform: {},
     testMatch: ['<rootDir>/tests/jest/*.test.js'],
     collectCoverageFrom: [
         'assets/js/**/*.js',
         'scripts/**/*.js',
         '!**/node_modules/**'
     ],
-    setupFilesAfterEnv: ['<rootDir>/tests/jest/setup.js'],
+    setupFilesAfterEnv: ['<rootDir>/tests/jest/setup.js']
 };
